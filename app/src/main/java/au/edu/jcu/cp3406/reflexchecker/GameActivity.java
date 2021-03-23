350b2ff6f2777f6122926c6cd14ecdecbe766b2e
Dallas-Marshall <dallas.marshall@my.jcu.edu.au>
2021-03-23 16:54:34 +1000

    Add addImage method

--- app/src/main/java/au/edu/jcu/cp3406/reflexchecker/GameActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/reflexchecker/GameActivity.java
@@ -3,12 +3,21 @@ package au.edu.jcu.cp3406.reflexchecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
-    private Random random = new Random();
+    private final Random random = new Random();
+    private static final int[] drawables = {
+            R.drawable.baseline_commute_black_48,
+            R.drawable.baseline_flight_land_black_48,
+            R.drawable.baseline_report_problem_black_48,
+            R.drawable.baseline_trending_down_black_48
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +27,10 @@ public class GameActivity extends AppCompatActivity {
         // Setup the task descriptions
         setupDescription(R.id.task1, R.array.task1_descriptions);
         setupDescription(R.id.task2, R.array.task2_descriptions);
+
+        for (int i = 0; i < 5; i++) {
+            addImage();
+        }
     }
 
     public void setupDescription(int taskID, int arrayID) {
@@ -26,5 +39,16 @@ public class GameActivity extends AppCompatActivity {
 
         int i = random.nextInt(descriptions.length);
         task.setText(descriptions[i]);
+    }
+
+    public void addImage() {
+        ViewGroup gameRows = findViewById(R.id.game_rows);
+        getLayoutInflater().inflate(R.layout.image, gameRows);
+
+        View lastChild = gameRows.getChildAt(gameRows.getChildCount() - 1);
+        ImageView image = lastChild.findViewById(R.id.image);
+
+        int index = random.nextInt(drawables.length);
+        image.setImageDrawable(getResources().getDrawableForDensity(drawables[index], 0));
     }
 }
